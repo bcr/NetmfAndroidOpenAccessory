fa375f79f0bfc042db40f36456af527188164f19
Blake Ramsdell <blaker@gmail.com>
2011-05-11 12:44:55 -0700

    Initial revision. Probably some junk in here.

---
+++ Prototype.AndroidOpenAccessory.Fez/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+using GHIElectronics.NETMF.FEZ;
+using GHIElectronics.NETMF.USBHost;
+
+namespace Prototype.AndroidOpenAccessory.Fez
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Running");
+            // Subscribe to USBH events.
+            USBHostController.DeviceConnectedEvent += DeviceConnectedEvent;
+            USBHostController.DeviceDisconnectedEvent += DeviceDisconnectedEvent;
+            Debug.Print("Listening for events");
+
+            // Sleep forever
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void DeviceConnectedEvent(USBH_Device device)
+        {
+            Debug.Print("Device connected...");
+            Debug.Print("ID: " + device.ID + ", Interface: " + device.INTERFACE_INDEX + ", Type: " + device.TYPE);
+            Debug.Print("Vendor ID: " + device.VENDOR_ID + ", Product ID:" + device.PRODUCT_ID);
+        }
+
+        static void DeviceDisconnectedEvent(USBH_Device device)
+        {
+            Debug.Print("Device disconnected...");
+            Debug.Print("ID: " + device.ID + ", Interface: " + device.INTERFACE_INDEX + ", Type: " + device.TYPE);
+            Debug.Print("Vendor ID: " + device.VENDOR_ID + ", Product ID:" + device.PRODUCT_ID);
+        }
+    }
+}
